f0ceb37bd48f58cf909ab0ed0fb4f8961f8bc112
Robinson <robinson@dorkbox.com>
2023-01-23 12:15:46 +0100

    updated description

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ plugins {
 
 object Extras {
     // set for the project
-    const val description = "Niche collections to augment what is already available."
+    const val description = "Finite State Machine using the AhoCorasick implementation, java 8+"
     const val group = "com.dorkbox"
     const val version = "1.0"
 
