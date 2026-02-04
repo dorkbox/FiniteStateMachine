5de765abb5451583b4ea5eadaddde74da0ad6f5e
Robinson <robinson@dorkbox.com>
2026-02-04 17:28:26 +0100

    build pipeline update

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,32 +24,39 @@ gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show th
 
 
 plugins {
-    id("com.dorkbox.GradleUtils") version "4.6"
+    id("com.dorkbox.GradleUtils") version "4.8"
     id("com.dorkbox.Licensing") version "3.1"
-    id("com.dorkbox.VersionUpdate") version "3.1"
-    id("com.dorkbox.GradlePublish") version "2.0"
+    id("com.dorkbox.VersionUpdate") version "3.2"
+    id("com.dorkbox.GradlePublish") version "2.2"
 
     kotlin("jvm") version "2.3.0"
 }
 
-object Extras {
-    // set for the project
-    const val description = "Finite State Machine using the AhoCorasick implementation using a Double Array Trie, java 8+"
-    const val group = "com.dorkbox"
-    const val version = "1.3"
 
-    // set as project.ext
-    const val name = "FSM"
-    const val id = "FSM" // this is the maven ID!
-    const val vendor = "Dorkbox LLC"
-    const val vendorUrl = "https://dorkbox.com"
-    const val url = "https://git.dorkbox.com/dorkbox/FSM"
+GradleUtils.load {
+    group = "com.dorkbox"
+    id = "FSM" // this is the maven ID!
+
+    description = "Finite State Machine using the AhoCorasick implementation using a Double Array Trie"
+    name = "FSM"
+    version = "1.3"
+
+    vendor = "Dorkbox LLC"
+    vendorUrl = "https://dorkbox.com"
+
+    url = "https://git.dorkbox.com/dorkbox/FSM"
+
+    issueManagement {
+        url = "${url}/issues"
+        nickname = "Gitea Issues"
+    }
+
+    developer {
+        id = "dorkbox"
+        name = vendor
+        email = "email@dorkbox.com"
+    }
 }
-
-///////////////////////////////
-/////  assign 'Extras'
-///////////////////////////////
-GradleUtils.load("$projectDir/../../gradle.properties", Extras)
 GradleUtils.defaults()
 GradleUtils.compileConfiguration(JavaVersion.VERSION_25)
 
@@ -84,32 +91,9 @@ tasks.jar.get().apply {
     }
 }
 
+
 dependencies {
     api("com.dorkbox:Updates:1.3")
 
     testImplementation("junit:junit:4.13.2")
-}
-
-mavenCentral {
-    groupId = Extras.group
-    artifactId = Extras.id
-    version = Extras.version
-
-    name = Extras.name
-    description = Extras.description
-    url = Extras.url
-
-    vendor = Extras.vendor
-    vendorUrl = Extras.vendorUrl
-
-    issueManagement {
-        url = "${Extras.url}/issues"
-        nickname = "Gitea Issues"
-    }
-
-    developer {
-        id = "dorkbox"
-        name = Extras.vendor
-        email = "email@dorkbox.com"
-    }
 }
